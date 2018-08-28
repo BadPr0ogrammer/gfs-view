923f8560945af98f995b5f640685bfb65fb34cbb
BadPr0ogrammer <42782684+BadPr0ogrammer@users.noreply.github.com>
2018-08-28 21:54:31 +0300

    Add files via upload

---
+++ src/met-5.2_bugfix/config.h
@@ -0,0 +1,267 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* "build ascii2nc" */
+#define ENABLE_ASCII2NC /**/
+
+/* "build ensemble_stat" */
+#define ENABLE_ENSEMBLE_STAT /**/
+
+/* "build gen_vx_mask" */
+#define ENABLE_GEN_VX_MASK /**/
+
+/* "build GRIB2 utilities" */
+/* #undef ENABLE_GRIB2 */
+
+/* "build grid_stat" */
+#define ENABLE_GRID_STAT /**/
+
+/* "build gsi_tools" */
+#define ENABLE_GSI_TOOLS /**/
+
+/* "build madis2nc" */
+#define ENABLE_MADIS2NC /**/
+
+/* "build mode" */
+#define ENABLE_MODE /**/
+
+/* "build mode_analysis" */
+#define ENABLE_MODE_ANALYSIS /**/
+
+/* "build mode_graphics" */
+/* #undef ENABLE_MODE_GRAPHICS */
+
+/* "build mode_time_domain" */
+#define ENABLE_MODE_TIME_DOMAIN /**/
+
+/* "build modis" */
+/* #undef ENABLE_MODIS */
+
+/* "build pb2nc" */
+#define ENABLE_PB2NC /**/
+
+/* "build pcp_combine" */
+#define ENABLE_PCP_COMBINE /**/
+
+/* "build plot_data_plane" */
+#define ENABLE_PLOT_DATA_PLANE /**/
+
+/* "build plot_point_obs" */
+#define ENABLE_PLOT_POINT_OBS /**/
+
+/* "build point_stat" */
+#define ENABLE_POINT_STAT /**/
+
+/* "build regrid_data_plane" */
+#define ENABLE_REGRID_DATA_PLANE /**/
+
+/* "build series_analysis" */
+#define ENABLE_SERIES_ANALYSIS /**/
+
+/* "build shift_data_plane" */
+#define ENABLE_SHIFT_DATA_PLANE /**/
+
+/* "build stat_analysis" */
+#define ENABLE_STAT_ANALYSIS /**/
+
+/* "build tc_utils" */
+#define ENABLE_TC_UTILS /**/
+
+/* "build wavelet_stat" */
+#define ENABLE_WAVELET_STAT /**/
+
+/* "build wwmca" */
+#define ENABLE_WWMCA /**/
+
+/* Define to 1 if you have the `dup2' function. */
+#define HAVE_DUP2 1
+
+/* Define to 1 if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the `floor' function. */
+/* #undef HAVE_FLOOR */
+
+/* Define to 1 if you have the `gethostname' function. */
+#define HAVE_GETHOSTNAME 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `isatty' function. */
+#define HAVE_ISATTY 1
+
+/* Define to 1 if you have the <limits.h> header file. */
+#define HAVE_LIMITS_H 1
+
+/* Define to 1 if your system has a GNU libc compatible `malloc' function, and
+   to 0 otherwise. */
+#define HAVE_MALLOC 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define to 1 if you have the `mkdir' function. */
+#define HAVE_MKDIR 1
+
+/* Define to 1 if you have the `pow' function. */
+/* #undef HAVE_POW */
+
+/* Define to 1 if your system has a GNU libc compatible `realloc' function,
+   and to 0 otherwise. */
+#define HAVE_REALLOC 1
+
+/* Define to 1 if you have the `regcomp' function. */
+#define HAVE_REGCOMP 1
+
+/* Define to 1 if you have the `select' function. */
+#define HAVE_SELECT 1
+
+/* Define to 1 if you have the `setenv' function. */
+#define HAVE_SETENV 1
+
+/* Define to 1 if you have the `sqrt' function. */
+/* #undef HAVE_SQRT */
+
+/* Define to 1 if you have the <stddef.h> header file. */
+#define HAVE_STDDEF_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strcasecmp' function. */
+#define HAVE_STRCASECMP 1
+
+/* Define to 1 if you have the `strchr' function. */
+#define HAVE_STRCHR 1
+
+/* Define to 1 if you have the `strcspn' function. */
+#define HAVE_STRCSPN 1
+
+/* Define to 1 if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strncasecmp' function. */
+#define HAVE_STRNCASECMP 1
+
+/* Define to 1 if you have the `strrchr' function. */
+#define HAVE_STRRCHR 1
+
+/* Define to 1 if you have the `strstr' function. */
+#define HAVE_STRSTR 1
+
+/* Define to 1 if you have the <sys/file.h> header file. */
+#define HAVE_SYS_FILE_H 1
+
+/* Define to 1 if you have the <sys/param.h> header file. */
+#define HAVE_SYS_PARAM_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if the system has the type `_Bool'. */
+#define HAVE__BOOL 1
+
+/* Name of package */
+#define PACKAGE "met"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "met_help@ucar.edu"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "MET"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "MET 5.2_bugfix"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "met"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://www.dtcenter.org/met/users"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "5.2_bugfix"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "5.2_bugfix"
+
+/* Define to 1 if `lex' declares `yytext' as a `char *' by default, not a
+   `char[]'. */
+/* #undef YYTEXT_POINTER */
+
+/* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT32_T */
+
+/* Define for Solaris 2.5.1 so the uint8_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT8_T */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to the type of a signed integer type of width exactly 16 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int16_t */
+
+/* Define to the type of a signed integer type of width exactly 32 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int32_t */
+
+/* Define to the type of a signed integer type of width exactly 8 bits if such
+   a type exists and the standard includes do not define it. */
+/* #undef int8_t */
+
+/* Define to rpl_malloc if the replacement function should be used. */
+/* #undef malloc */
+
+/* Define to `long int' if <sys/types.h> does not define. */
+/* #undef off_t */
+
+/* Define to rpl_realloc if the replacement function should be used. */
+/* #undef realloc */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to the type of an unsigned integer type of width exactly 16 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint16_t */
+
+/* Define to the type of an unsigned integer type of width exactly 32 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint32_t */
+
+/* Define to the type of an unsigned integer type of width exactly 8 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint8_t */
